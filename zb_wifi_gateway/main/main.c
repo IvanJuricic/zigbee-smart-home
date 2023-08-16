c223305840b65386f4fee5e66974ff2f9eeb7c93
IvanJuricic <ivanjuricic97@gmail.com>
2023-08-16 22:32:38 +0200

    Add socket connectivity

--- zb_wifi_gateway/main/main.c
+++ zb_wifi_gateway/main/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 static const char *TAG = "Zigbee-Wifi Gateway";
+int counter = 1;
 
 void app_main(void)
 {
@@ -25,8 +26,50 @@ void app_main(void)
 
 void tcp_client_task(void *pvParameters) {
 
-    for(;;) {
-        ESP_LOGI(TAG, "TCP task!\n");
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr("192.168.1.10"); // Replace with your server's local IP address
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(12345); // Replace with your server port
+
+    int sock;
+
+    ESP_LOGI(TAG, "Initializing socket connection....!\n");
+
+    while(1) {
+        sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Failed to create socket");
+            continue;
+        }
+
+        ESP_LOGI(TAG, "Socket created!\n");
+
+        if (connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in)) != 0) {
+            ESP_LOGE(TAG, "Socket connection failed");
+            close(sock);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "Socket created and connected!!!\n");
+        break;
+    }
+
+    while (counter < 6) {
+        const char *data = "Hello, server from esp32 hahahaha!";
+        send(sock, data, strlen(data), 0);
+        ESP_LOGI(TAG, "Data sent to server: %s", data);
+
+        char recv_buf[64];
+        int recv_len = recv(sock, recv_buf, sizeof(recv_buf) - 1, 0);
+        if (recv_len > 0) {
+            recv_buf[recv_len] = '\0';
+            ESP_LOGI(TAG, "Received from server: %s", recv_buf);
+        }
+
+        counter++;
+
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }   
+    }
+
+    close(sock);
 }
--- zb_wifi_gateway/main/main.h
+++ zb_wifi_gateway/main/main.h
@@ -7,7 +7,9 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_netif.h"
 
+#include "lwip/sockets.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
